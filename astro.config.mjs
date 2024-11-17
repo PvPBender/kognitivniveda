d57031b8356d3e1ad604291fb095faa68cb1c73f
PvPBender <me@pvpbender.com>
2024-11-17 20:28:49 +0100

    bolt.new 8

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://pvpbender.github.io',
+  base: '/kognitivniveda',
+  outDir: './dist',
   build: {
     assets: 'assets'
   }
