5ae6654e78564cfe6554bd0880aa8849fef1769d
PvPBender <me@pvpbender.com>
2024-11-17 20:18:45 +0100

    bolt.new 7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  outDir: './docs',
-  base: '', // This will be updated when you know your repo name
   build: {
     assets: 'assets'
   }
